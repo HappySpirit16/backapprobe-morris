7fac5465969a11a127ad8425e701fb2d3d4eb9dd
Josué Urcia <jurcia@morrisopazo.com>
2025-06-04 10:35:02 -0500

    up

--- iac/bin/cdk.ts
+++ iac/bin/cdk.ts
@@ -7,20 +7,21 @@ const app = new cdk.App();
 
 const environment = "dev";
 
-const project = "morris"
-const region = "us-east-1"
+const project = "morris";
+const repo = "backapprobe";
 
 export function createName(resource: string, functionality: string, caseType: string = "kebab") {
-	if (caseType === "camel") {
-		function capF(val: string) {
-			return String(val).charAt(0).toUpperCase() + String(val).slice(1);
-		}
-		return `${capF(project)}${capF(region)}${capF(resource)}${capF(environment)}${capF(functionality)}`
-			.split('-').join('');
-	} if (caseType === "nodash") {
-		return `${project}${region}${resource}${environment}${functionality}`;
-	} else {
-		return `${project}-${region}-${resource}-${environment}-${functionality}`;
-	}
+  if (caseType === "camel") {
+    function capF(val: string) {
+      return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    }
+    return `${capF(project)}${capF(resource)}${capF(environment)}${capF(functionality)}`
+      .split('-').join('');
+  } else if (caseType === "nodash") {
+    return `${project}${resource}${environment}${functionality}`;
+  } else {
+    return `${project}-${resource}-${environment}-${functionality}`;
+  }
 }
-new CdkStack(app, createName("dw", "stack"));
+
+new CdkStack(app, createName(repo, "stack"));
